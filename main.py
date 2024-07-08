c79477f981cb223013d179bcdbe0c025fd44689e
Sasa Salmen <sasinhe@gmail.com>
2024-07-08 20:47:10 -0300

    Functional code!!

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+from multiprocessing import Pool
 
 CHARACTERS = ".:-=+*≡#@" # The reference array for substituting pixels with characters, in decreasing brightness (. = lightest pixel, @ = darkest pixel)
 
@@ -11,8 +12,14 @@ def getCharacter(brightness_value: float, brightness_interval: list[float, float
 
     interval_size = brightness_interval[1] - brightness_interval[0]
     step = interval_size / len(CHARACTERS)
-    index = int((brightness_value - brightness_interval[0]) / step)
-    
+
+    # This is bad practice!! But there is a problem when the brightness_value = max brightness, so that it's mapped outside of the array. This is the best solution I could think at the moment.
+    if brightness_value == brightness_interval[1]:
+        return CHARACTERS[-1]
+    else:
+        index = int((brightness_value - brightness_interval[0]) / step)
+
+
     return CHARACTERS[index] 
 
 
@@ -32,6 +39,20 @@ def main():
                 R,G,B = pixelRGB
                 brightness = sum([R,G,B])/3
                 brightArray[i,j] = brightness
+
+    
+    Output = ''
+    brightInterval = [np.min(brightArray), np.max(brightArray)]
+    
+    for line in brightArray:
+        for val in line:
+            Output += getCharacter(val, brightInterval)
+        Output += '\n'
+
+    print(Output)
+
+
+
 
 
 
